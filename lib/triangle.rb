ca942097535b1821a432303b9dd7c2c697f61366
Damely Tineo <damely.tineo07@gmail.com>
2020-01-26 06:11:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,9 @@ class Triangle
   
   def kind 
     if((@l1<=0)||(@l2<=0)||(@l3<=0)) || ((@l1+@l2>@l3)||(@l1+@l3>@l2)||(@l2+@l3>@l1))
-      begin
+      
         raise TriangleError
-      end
+      
     
     elsif (@l1 == @l2 && @l2 == @l3)
       :equilateral
