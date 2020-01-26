ba2dd8c7060bd867a8006d1cff0796cf9ca164c5
Damely Tineo <damely.tineo07@gmail.com>
2020-01-26 06:37:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,8 @@ class Triangle
   
   def kind 
    
-    if (@l1<=0) || (@l2<=0) || (@l3<=0) #||
-      #(@l1+@l2>@l3) || (@l1+@l3>@l2) || (@l2+@l3>@l1)
+    if (@l1<=0) || (@l2<=0) || (@l3<=0) ||
+      !((@l1+@l2>@l3) || (@l1+@l3>@l2) || (@l2+@l3>@l1))
       raise TriangleError
     end
     
